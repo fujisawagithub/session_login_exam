6512bd6b67a7ff6f138ba6a3e233bacbc9ed80f1
yuki fujisawa <fujisawa.yuki@outlook.com>
2021-11-12 11:38:36 +0900

    ログアウト機能実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
-class ApplicationController < ActionController::
-  include SessionsHelper
-end
+class ApplicationController < ActionController::Base
+    protect_from_forgery with: :exception
+    include SessionsHelper
+    private
+    def login_required
+      redirect_to new_session_path unless current_user
+    end
+  end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,4 +2,11 @@ module SessionsHelper
   def create
     user = User.find_by(email: params[:session][:email].downcase)
   end
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+  def logged_in?
+    current_user.present?
+  end
 end
+
